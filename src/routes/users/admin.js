dc8683ef5f31a08b5d0b33353596da439604bd94
Nikhil Chitte <chnikhil250@gmail.com>
2022-10-05 02:19:44 +0530

    update roles and bug fixes

--- src/routes/users/admin.js
+++ src/routes/users/admin.js
@@ -1,14 +1,24 @@
 const user = require('../../models/user');
 const mongoose = require('mongoose');
 const router = require('express').Router();
+const roles = require('../../utils/constants');
 
-
-router.get('/admin', async(req, res, next)=> {
+async function roleCheck(req, res, next){
     try {
         const users = await user.find();
         (req.user.role=="admin") ? res.render('changeRole', {users: users}) : res.send('Access Denied!!');
     } catch (error) {
-        next(error)
+        next(error);
+    } 
+} 
+
+router.get('/admin', async(req, res, next)=> {
+    if(!req.user){
+            req.flash('alert-danger', 'You have to login first');
+            res.render('login', {messages: req.flash()});
+        }
+    else{
+        roleCheck(req, res, next);
     }
 });
 
@@ -25,6 +35,40 @@ router.get('/:id', async(req, res, next)=>{
     } catch (err) {
         console.error(err);
         next(err);
+    }
+});
+
+
+router.post('/admin/update', async(req, res, next)=>{
+    const {id, role} = req.body;
+    const users = await user.find();
+    if(!id || !role){
+        req.flash('alert-danger', 'Invalid Request!! ');
+        res.render('changeRole', {messages: req.flash(), users: users});
+    }
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        req.flash('alert-danger', 'Invalid request');
+        res.render('changeRole', {messages: req.flash(), users: users});
+    }
+    const rolesArr = Object.values(roles);
+    if(!rolesArr.includes(role)){
+        req.flash('alert-danger', 'Invalid request');
+        res.render('changeRole', {messages: req.flash(), users: users});
+    }
+    if(req.user.id==id){
+        req.flash('alert-danger', 'Ask another admin');
+        res.render('changeRole', {messages: req.flash(), users: users});
+    }
+    const userRole = await user.findById(id);
+    if(role==userRole.role){
+        req.flash('alert-danger', `Already a ${userRole.role}`);
+        res.render('changeRole', {messages: req.flash(), users: users});
+    }
+    else{
+        //update role
+        const newRoleUser = await user.findByIdAndUpdate(id, {role: role}, {new: true, runValidators: true});
+        req.flash('alert-success', 'Role Changed Sucessfully!!');
+        res.render('changeRole', {messages: req.flash(), users: await user.find()});
     }
 });
 
