e8c2e71d8f9f56c10fe70674c79d823e18702a2b
Nikhil Chitte <chnikhil250@gmail.com>
2022-10-24 17:10:11 +0530

    db config update

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    // db_url: `mongodb+srv://expressrbac:${process.env.password}@rbac.pq3evnb.mongodb.net/?retryWrites=true&w=majority`,
-    db_url: 'mongodb://localhost:27017/',
+    db_url: `mongodb+srv://expressrbac:${process.env.password}@rbac.pq3evnb.mongodb.net/?retryWrites=true&w=majority`,
+    // db_url: 'mongodb://localhost:27017/',
     db_name: 'rbac',
     cookie_options: {
         httpOnly: true, sameSite: 'strict', secure: true, path: "/"
