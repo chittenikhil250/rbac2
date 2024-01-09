775dd907dee2e5092f1121e4e2e702df8399d5f4
Nikhil Chitte <chnikhil250@gmail.com>
2024-01-09 19:55:15 +0530

    deploy

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -10,7 +10,6 @@ router.get('/login', (req, res)=> {
 router.post('/login', (req, res, next)=>{
 
     passport.authenticate('local', { session: false }, (err, user, info)=>{
-            
             if(err){
                 return next(err);
             }
@@ -20,7 +19,7 @@ router.post('/login', (req, res, next)=>{
                 })
             }
             const token = generateToken(user);
-            res.cookie('token', token,  { httpOnly: true });
+            res.cookie('token', token,  { httpOnly: true, secure: true });
             return res.status(200).json({
                 message: 'Login Succesfull',
                 user: user
@@ -30,10 +29,6 @@ router.post('/login', (req, res, next)=>{
 });
 
 router.get('/logout', async function(req, res, next){
-    // req.logout((err) => {
-    //     if (err) { return next(err);}
-    //     return res.status(200).json({message: 'Logout Succesfull!!'})
-    //   });
     try {
         res.clearCookie('token');
         return res.status(200).json({message: 'Logout Succesfull'});
