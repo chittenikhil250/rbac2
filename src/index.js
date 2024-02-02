7146698659b9af07474328756e593465acc23773
Nikhil Chitte <84327788+chittenikhil250@users.noreply.github.com>
2024-02-02 22:38:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ require('dotenv').config();
 const port = process.env.port || 3000;
 
 const corsOptions = {
-    origin: 'https://rbacfrontend.vercel.app/',
+    origin: 'https://rbacfrontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }
